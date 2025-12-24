3372d312a0a498cea3ce118f7ff4a6adc7906142
Zoro07 <nadipallijaswanth541@gmail.com>
2025-12-24 21:33:23 +0530

    added the origin

--- Backend/index.js
+++ Backend/index.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: "https://social-media-nu-two.vercel.app/", credentials: true }));
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
